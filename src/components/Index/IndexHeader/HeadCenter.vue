1c6408fd76433d80baefc63d9397299905333343
LCShao <2859594131@qq.com>
2017-12-13 18:35:32 +0800

    20171213

---
+++ src/components/Index/IndexHeader/HeadCenter.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="headCenter">
+    <div class="sbox">
+      <i></i>
+      <input type="text"/>
+      <a>搜索</a>
+    </div>
+    <ul>
+      <li><span>大家都在搜:</span></li>
+      <li><a href="#">微整不动刀</a></li>
+      <li><a href="#">大牌名医</a></li>
+      <li><a href="#">玻尿酸</a></li>
+      <li><a href="#">水光针</a></li>
+      <li><a href="#">祛眼袋</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'head-center'
+  }
+</script>
+
+<style lang="less">
+  .headCenter{
+    ul{
+      margin-top:10px;
+      li{
+        float:left;
+        margin-left:5px;
+        a,span{
+          font-size:14px;
+          color:#9B9B9B;
+        }
+      }
+    }
+    .sbox{
+      width:382px;
+      height:32px;
+      border-radius:30px;
+      border:1px solid #2CC7C5;
+      margin-top:40px;
+      i{
+        display:block;
+        float:left;
+        width:16px;
+        height:16px;
+        background:url("../../../assets/img/images/img.png");
+        background-position:-114px 0px;
+        margin:8px;
+      }
+      input{
+        border:none;
+        outline:none;
+        height:30px;
+        width:290px;
+      }
+      a{
+        display:block;
+        float:right;
+        margin-top:7px;
+        margin-right:5px;
+        padding-left:2px;
+        height:20px;
+        width:50px;
+        border-left:1px solid #2CC7C5;
+        background:#fff;
+        font-size:14px;
+        color:#2CC7C5;
+        &:hover{
+          cursor:pointer;
+        }
+      }
+    }
+  }
+</style>
--- src/components/Index/IndexHeader/IndexHeader.vue
+++ src/components/Index/IndexHeader/IndexHeader.vue
@@ -31,21 +31,7 @@
     </div>
     <div class="head">
       <img :src="logo"/>
-      <div class="headCenter">
-        <div class="sbox">
-          <i></i>
-          <input type="text"/>
-          <a>搜索</a>
-        </div>
-        <ul>
-          <li><span>大家都在搜:</span></li>
-          <li><a href="#">微整不动刀</a></li>
-          <li><a href="#">大牌名医</a></li>
-          <li><a href="#">玻尿酸</a></li>
-          <li><a href="#">水光针</a></li>
-          <li><a href="#">祛眼袋</a></li>
-        </ul>
-      </div>
+      <head-center></head-center>
       <div class="conr">
         <i></i>
         <p>中国整形美容协会<br>
@@ -58,16 +44,16 @@
 
 <script>
   import UserStatus from '@/components/User/UserStatus'
+  import HeadCenter from '@/components/Index/IndexHeader/HeadCenter'
   export default {
     name: 'index-header',
     data () {
       return {
         logo: '/src/assets/img/login/logo.png'
-
       }
     },
     components: {
-      UserStatus
+      UserStatus, HeadCenter
     }
   }
 </script>
@@ -160,57 +146,7 @@
           margin-left:5px;
         }
       }
-      .headCenter{
-        ul{
-          margin-top:10px;
-          li{
-            float:left;
-            margin-left:5px;
-            a,span{
-              font-size:14px;
-              color:#9B9B9B;
-            }
-          }
-        }
-        .sbox{
-          width:382px;
-          height:32px;
-          border-radius:30px;
-          border:1px solid #2CC7C5;
-          margin-top:40px;
-          i{
-            display:block;
-            float:left;
-            width:16px;
-            height:16px;
-            background:url("../../../assets/img/images/img.png");
-            background-position:-114px 0px;
-            margin:8px;
-          }
-          input{
-            border:none;
-            outline:none;
-            height:30px;
-            width:290px;
-          }
-          a{
-            display:block;
-            float:right;
-            margin-top:7px;
-            margin-right:5px;
-            padding-left:2px;
-            height:20px;
-            width:50px;
-            border-left:1px solid #2CC7C5;
-            background:#fff;
-            font-size:14px;
-            color:#2CC7C5;
-            &:hover{
-              cursor:pointer;
-            }
-          }
-        }
-      }
+
     }
   }
 </style>
