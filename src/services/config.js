1a2a5c37ff4d7700aaaecc4ad0817bc3064aedd6
LCShao <2859594131@qq.com>
2017-12-23 21:20:21 +0800

    20171223

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,5 @@
 export const baseHost =
 //  'http://localhost/project01/data' // php地址
-//  'http://angilla.applinzi.com/data/' // 新浪云地址
-'http://localhost:3000' // 本地node地址
+//  'http://angilla.applinzi.com/data/' // 新浪云php地址
+// 'http://localhost:3000' // 本地node地址
+'http://nodeangilla.applinzi.com'
